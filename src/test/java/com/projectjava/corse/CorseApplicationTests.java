4fa52e655a78209f823a21aa7781915a3fde6d61
NatanCristhoffe <natan.cristhoffe@outlook.com>
2025-12-15 13:38:59 -0300

    Project created

---
+++ src/test/java/com/projectjava/corse/CorseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectjava.corse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
