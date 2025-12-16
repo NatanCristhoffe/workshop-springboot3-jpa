5ebc2b926ed5e1c9adbb1cfab5972c0d675138c2
NatanCristhoffe <natan.cristhoffe@outlook.com>
2025-12-16 07:22:23 -0300

    Order, Instant, ISO 8601 - PART 2

---
+++ src/main/java/com/projectjava/corse/services/OrderService.java
@@ -0,0 +1,25 @@
+package com.projectjava.corse.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projectjava.corse.entities.Order;
+import com.projectjava.corse.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll(){
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id){
+		Optional<Order> Order = repository.findById(id);
+		return Order.get();
+	}
+}
