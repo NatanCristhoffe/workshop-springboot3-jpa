9fbaeb37b83e9fa036c738ec26ca2452a1e7d87f
NatanCristhoffe <natan.cristhoffe@outlook.com>
2025-12-16 14:08:42 -0300

    Payment, one-to-one association

--- src/main/java/com/projectjava/corse/config/TestConfig.java
+++ src/main/java/com/projectjava/corse/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.projectjava.corse.entities.Category;
 import com.projectjava.corse.entities.Order;
 import com.projectjava.corse.entities.OrderItem;
+import com.projectjava.corse.entities.Payment;
 import com.projectjava.corse.entities.Product;
 import com.projectjava.corse.entities.User;
 import com.projectjava.corse.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:00:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
+		
 	}
 	
 }
